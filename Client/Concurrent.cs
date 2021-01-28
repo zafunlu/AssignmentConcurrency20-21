2d0ae67bf0af5a0e248264717f18f00ff475cc53
Zafer <0970135@hr.nl>
2021-01-28 23:02:22 +0100

    Working threading part

--- Client/Concurrent.cs
+++ Client/Concurrent.cs
@@ -11,10 +11,18 @@ namespace Concurrent
         public ConcurrentClient(int id, Setting settings) : base(id, settings)
         {
             // todo [Assignment]: implement required code
+            workerThread = new Thread(run);
+            workerThread.Start();
         }
         public void run()
         {
-            // todo [Assignment]: implement required code
+            String[] votingList = settings.votingList.Split(settings.commands_sep);
+            Random rnd = new Random();
+
+            cmd = votingList[rnd.Next(votingList.Length)];
+            cmd_message = "ClientId="+client_id.ToString()+settings.command_msg_sep+cmd;
+            prepareClient();
+            communicate();
         }
     }
     public class ConcurrentClientsSimulator : SequentialClientsSimulator
@@ -23,6 +31,7 @@ namespace Concurrent
 
         public ConcurrentClientsSimulator() : base()
         {
+            configure();
             Console.Out.WriteLine("\n[ClientSimulator] Concurrent simulator is going to start with {0}", settings.experimentNumberOfClients);
             clients = new ConcurrentClient[settings.experimentNumberOfClients];
         }
@@ -32,6 +41,15 @@ namespace Concurrent
             try
             {
                 // todo [Assignment]: implement required code
+                for (int i = 0; i < clients.Length; i++)
+                {
+                    clients[i] = new ConcurrentClient(i, settings);
+                }
+
+                for (int i = 0; i < clients.Length; i++)
+                {
+                    clients[i].workerThread.Join();
+                }
             }
             catch (Exception e)
             { Console.Out.WriteLine("[Concurrent Simulator] {0}", e.Message); }
--- Server/Concurrent.cs
+++ Server/Concurrent.cs
@@ -1,5 +1,9 @@
 using Sequential;
 using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
 //todo [Assignment]: add required namespaces
 
 namespace Concurrent
@@ -7,19 +11,58 @@ namespace Concurrent
     public class ConcurrentServer : SequentialServer
     {
         // todo [Assignment]: implement required attributes specific for concurrent server
-
+        public List<Thread> workerThreads;
         public ConcurrentServer(Setting settings) : base(settings)
         {
             // todo [Assignment]: implement required code
+            workerThreads = new List<Thread>();
         }
         public override void prepareServer()
         {
-            // todo [Assignment]: implement required code
+            Console.WriteLine("[Server] is ready to start ...");
+            try
+            {
+                localEndPoint = new IPEndPoint(this.ipAddress, settings.serverPortNumber);
+                listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                listener.Bind(localEndPoint);
+                listener.Listen(settings.serverListeningQueue);
+                Console.WriteLine("Waiting for incoming connections ... ");
+                while (true)
+                {
+                    Socket connection = listener.Accept();
+                    this.numOfClients++;
+                    var t = new Thread(() => {
+                        handleClient(connection);
+                    });
+                    t.Start(connection);
+                    workerThreads.Add(t);
+                }
+            }catch (Exception e){ Console.Out.WriteLine("[Server] Preparation: {0}",e.Message); }
         }
         public override string processMessage(String msg)
         {
-            // todo [Assignment]: implement required code
-            return "";
+            string replyMsg = Message.confirmed;
+
+            try
+            {
+                switch (msg)
+                {
+                    case Message.terminate:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("[Server] received from the client -> {0} ", msg);
+                        Console.ResetColor();
+                        Console.WriteLine("[Server] END : number of clients communicated -> {0} ", this.numOfClients);
+                        break;
+                    default:
+                        replyMsg = Message.confirmed;
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("[Server] received from the client -> {0} ", msg);
+                        Console.ResetColor();
+                        break;
+                }
+            }catch (Exception e){   Console.Out.WriteLine("[Server] Process Message {0}", e.Message);    }
+
+            return replyMsg;
         }
     }
 }
